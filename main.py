080df4afbe96b17f0edc8e5228c9bcb9c210a0d6
DEN <dvgv35@gmail.com>
2021-04-26 16:21:26 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -38,5 +38,39 @@ def image():
                 </html>"""
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, марс!</h1>
+                    <img src="/static/img/mars.jpg" alt="Морс">
+                    <div class="alert alert-info" role="alert">
+                      Сейчас я прыгну
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      На гараж
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      Скоро космос
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Будет наш!
+                    </div>
+                    <br>конец стиха
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
